ce1bd79ecd8c39d97aa48ac939c8dc9745a3b551
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-09-18 06:43:49 +0900

    button

--- src/pages/DemoEarnPage/DemoEarn.tsx
+++ src/pages/DemoEarnPage/DemoEarn.tsx
@@ -196,14 +196,13 @@ const DemoDailyRewardComponent = ({
     }
     return (
         <div className={`h-[100px] cursor-pointer`}
-            aria-disabled={allowed == false}
             onClick={() => { // FIXME: add daily check in boolean field on each day on backend table 
                 handleCheckInDailyReward()
                 // setDailyReward(false)
                 // return setIsTodayCheckedIn(true)
             }}>
-            <div className='text-center w-[100%] h-[80px]' aria-disabled={allowed == false}>
-                <div aria-disabled={allowed == false} className={`relative w-[160px] h-14 rounded-[6px_6px_0px_0px] 
+            <div className='text-center w-[100%] h-[80px]'>
+                <div className={`relative w-[160px] h-14 rounded-[6px_6px_0px_0px] 
                 ${allowed == false ? "[background:linear-gradient(180deg,rgb(169,231,29)_0%,rgb(94.04,196.56,89.27)_100%)]" :
                         "[background:radial-gradient(50%_50%_at_50%_50%,rgb(112.62,108.57,77.9)_0%,rgb(119,102.27,78.84)_100%)]"}`}>
                     {allowed == false ? <div className="absolute w-[77px] top-[7px] left-[40px] [font-family:'Roboto-Medium',Helvetica] font-medium text-[#ffffff] text-xl text-center tracking-[0] leading-[22px]">
