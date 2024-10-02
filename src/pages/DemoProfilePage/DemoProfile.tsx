43e432c59b5f81cc5dbe5741689893e6be0449ab
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-02 15:17:29 +0900

    loading when it is need, rm fallback case

--- src/pages/DemoProfilePage/DemoProfile.tsx
+++ src/pages/DemoProfilePage/DemoProfile.tsx
@@ -32,14 +32,13 @@ const DemoProfile = () => {
     return (
         <ToastProvider swipeDirection='up'>
             <div>
-                {isLoading == true ?
-                    <Loader isLoading={isLoading} wrapperHeight='690px' spinnerTopPosition='12%' /> :
-                    <div className='text-white grid grid-cols-1'>
-                        <input
-                            placeholder='username'
-                            defaultValue={account?.telegram_info.username}
-                            type="text"
-                            className='mx-auto 
+                {isLoading == true && <Loader isLoading={isLoading} wrapperHeight='690px' spinnerTopPosition='12%' />}
+                <div className='text-white grid grid-cols-1'>
+                    <input
+                        placeholder='username'
+                        defaultValue={account?.telegram_info.username}
+                        type="text"
+                        className='mx-auto 
                             rounded-[8px] 
                             py-[19px] 
                             px-[25px] 
@@ -55,10 +54,10 @@ const DemoProfile = () => {
                             gap-10
                             bg-[rgba(255,255,255,0.20)]
                             m-3'
-                            onChange={(e) => setUsername(e.target.value)} />
-                        <Button
-                            variant='default'
-                            className='mx-auto text-white font-bold
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <Button
+                        variant='default'
+                        className='mx-auto text-white font-bold
                             [background:linear-gradient(158deg,rgba(169,231,29,1)_-7.35%,rgba(94,197,89,1)_84.4%)]
                             w-[343px]
                             h-[56px]
@@ -66,61 +65,60 @@ const DemoProfile = () => {
                             justify-center
                             items-center
                             rounded-[6px]'
-                            type='submit'
-                            onClick={async () => {
-                                if (import.meta.env.VITE_MINI_APP_ENV == 'test') {
-                                    setIsLoading(true)
-                                    if (username == account?.telegram_info.username) {
-                                        setIsLoading(false)
-                                        toast({
-                                            className: cn('bg-[#FFFAE6] rounded-[10px]'),
-                                            title: 'no change',
-                                            description: 'Username is not changed',
-                                            action: <ToastAction altText="Try again">Try again</ToastAction>,
+                        type='submit'
+                        onClick={async () => {
+                            if (import.meta.env.VITE_MINI_APP_ENV == 'test') {
+                                setIsLoading(true)
+                                if (username == account?.telegram_info.username) {
+                                    setIsLoading(false)
+                                    toast({
+                                        className: cn('bg-[#FFFAE6] rounded-[10px]'),
+                                        title: 'no change',
+                                        description: 'Username is not changed',
+                                        action: <ToastAction altText="Try again">Try again</ToastAction>,
 
-                                        })
-                                    }
-
-                                    else {
-                                        setIsLoading(false)
-                                        toast({
-                                            className: cn('bg-[#FFFAE6] rounded-[10px]'),
-                                            description:
-                                                <div className=''>
-                                                    Username is changed to { }
-                                                    <span className='font-semibold'>{username}</span>
-                                                </div>
-                                        })
-
-                                    }
-                                } else {
-                                    setIsLoading(true)
-                                    if (username == account?.telegram_info.username) {
-                                        setIsLoading(false)
-                                        toast({
-                                            className: cn('bg-[#FFFAE6] rounded-[10px]'),
-                                            title: 'no change',
-                                            description: 'Username is not changed',
-                                            action: <ToastAction altText="Try again">Try again</ToastAction>,
-                                        })
-                                    }
-                                    else {
-                                        await handleUsernameChange(username)
-                                        toast({
-                                            className: cn('bg-[#FFFAE6] rounded-[10px]'),
-                                            description:
-                                                <div className=''>
-                                                    Username is changed to { }
-                                                    <span className='font-semibold'>{username}</span>
-                                                </div>
-                                        })
-                                    }
+                                    })
                                 }
-                            }}>
-                            <span className='text-white text-[20px] font-bold text-center'>SAVE</span>
-                        </Button>
-                    </div >
-                }
+
+                                else {
+                                    setIsLoading(false)
+                                    toast({
+                                        className: cn('bg-[#FFFAE6] rounded-[10px]'),
+                                        description:
+                                            <div className=''>
+                                                Username is changed to { }
+                                                <span className='font-semibold'>{username}</span>
+                                            </div>
+                                    })
+
+                                }
+                            } else {
+                                setIsLoading(true)
+                                if (username == account?.telegram_info.username) {
+                                    setIsLoading(false)
+                                    toast({
+                                        className: cn('bg-[#FFFAE6] rounded-[10px]'),
+                                        title: 'no change',
+                                        description: 'Username is not changed',
+                                        action: <ToastAction altText="Try again">Try again</ToastAction>,
+                                    })
+                                }
+                                else {
+                                    await handleUsernameChange(username)
+                                    toast({
+                                        className: cn('bg-[#FFFAE6] rounded-[10px]'),
+                                        description:
+                                            <div className=''>
+                                                Username is changed to { }
+                                                <span className='font-semibold'>{username}</span>
+                                            </div>
+                                    })
+                                }
+                            }
+                        }}>
+                        <span className='text-white text-[20px] font-bold text-center'>SAVE</span>
+                    </Button>
+                </div >
             </div >
         </ToastProvider >
     )
