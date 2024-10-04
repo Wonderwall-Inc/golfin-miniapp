674fd9bd90694215499d984e76446239c188c8b5
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-04 15:10:02 +0900

    reducing size on css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const konstaConfig = require('konsta/config');
 const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = konstaConfig({
+	purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	darkMode: ["class"],
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
