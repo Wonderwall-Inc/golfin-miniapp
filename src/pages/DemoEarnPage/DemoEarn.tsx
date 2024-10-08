9f3c1a785c5aac6ddd131e30be01fd753c00bff1
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-08 18:08:12 +0900

    reorder the weekly after daily

--- src/pages/DemoEarnPage/DemoEarn.tsx
+++ src/pages/DemoEarnPage/DemoEarn.tsx
@@ -219,14 +219,7 @@ const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }:
                     user_id: account?.id,
                     access_token: ''
                 })
-                if (dailyCheckIn?.activity) {
-                    setActivity(dailyCheckIn.activity)
-                }
-                if (dailyCheckIn?.point) {
-                    setPoint(dailyCheckIn.point)
-                }
-
-                if (dailyCheckIn?.activity.login_streak == 7) {
+                if (dailyCheckIn?.activity && dailyCheckIn.activity.login_streak == 7) {
                     const weeklyCheckIn = await weeklyCheckInActivity({
                         user_id: account?.id,
                         access_token: ''
@@ -236,6 +229,12 @@ const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }:
                     }
                     if (weeklyCheckIn?.point) {
                         setPoint(weeklyCheckIn.point)
+                    }
+                } else {
+                    if (dailyCheckIn?.activity)
+                        setActivity(dailyCheckIn.activity)
+                    if (dailyCheckIn?.point) {
+                        setPoint(dailyCheckIn.point)
                     }
                 }
             }
