3a4836bce74c347171c313b89c84ab87f3b7e4e4
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-08 18:14:23 +0900

    move uef to the component

--- src/pages/DemoEarnPage/DemoEarn.tsx
+++ src/pages/DemoEarnPage/DemoEarn.tsx
@@ -186,7 +186,7 @@ const DemoEarnComponent = ({ timeLeft, totalPointAmount, sgTime, isClicked, setI
 
 
 const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }: DemoDailyRewardComponentProp) => {
-    const { setPoint, setIsWaitingPoint } = usePointContext()
+    const { setPoint, setIsWaitingPoint, point } = usePointContext()
     const { account } = useUserContext()
     const { setActivity, activity, setIsWaitingActivity } = useActivityContext()
     const [allowed, setAllowed] = useState(true)
@@ -210,6 +210,39 @@ const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }:
     }, [activity?.last_action_time])
 
 
+
+    useEffect(() => {
+        const handleWeeklyReward = async () => {
+            if (!activity?.login_streak || activity?.login_streak == 0 || activity?.login_streak !== 7) return; // Early exit if not a streak of 7
+
+            setIsWaitingActivity(true);
+            setIsWaitingPoint(true);
+            try {// Fetch existing point and update if necessary
+                if (activity && point) {
+                    if (account?.id) {
+                        const weeklyCheckIn = await weeklyCheckInActivity({
+                            user_id: account?.id,
+                            access_token: ''
+                        })
+                        if (weeklyCheckIn?.activity) {
+                            setActivity(weeklyCheckIn.activity)
+                        }
+                        if (weeklyCheckIn?.point) {
+                            setPoint(weeklyCheckIn.point)
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error('Error handling weekly reward:', error);
+            } finally {
+                setIsWaitingActivity(false);
+                setIsWaitingPoint(false);
+            }
+        };
+
+        handleWeeklyReward(); // Call the function on component mount
+    }, [activity?.login_streak]);
+
     const handleCheckInDailyReward = async () => {
         setIsWaitingActivity(true)
         setIsWaitingPoint(true)
@@ -224,7 +257,7 @@ const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }:
                 if (dailyCheckIn?.point) {
                     setPoint(dailyCheckIn.point)
                 }
-
+/* 
                 if (activity?.login_streak == 7) {
                     const weeklyCheckIn = await weeklyCheckInActivity({
                         user_id: account?.id,
@@ -236,7 +269,7 @@ const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }:
                     if (weeklyCheckIn?.point) {
                         setPoint(weeklyCheckIn.point)
                     }
-                }
+                } */
             }
         } catch (error) {
             console.error('Error processing daily check-in:', error);
