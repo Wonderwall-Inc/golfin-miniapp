c2cf921f5e672e05e4e6f9c4de12df85b71c3aa6
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-08 16:29:31 +0900

    add timer

--- src/pages/DemoEarnPage/DemoEarn.tsx
+++ src/pages/DemoEarnPage/DemoEarn.tsx
@@ -357,11 +357,12 @@ const DemoDailyRewardComponent = ({ timeLeft, sgTime, isClicked, setIsClicked }:
                     </div> :
                         <div className="absolute w-[123px] top-[7px] left-[19px] [font-family:'Roboto-Medium',Helvetica] font-medium text-[#ffffff] text-xl text-center tracking-[0] leading-[22px]"
                         >
-                            Daily Reward
+                           {/*  Daily Reward */}
+                           <span>{h}: {m}: {s}</span>
                             <br />
 
                             <Countdown targetDate={timeLeft}  /* dailyReward={dailyReward} setDailyReward={setDailyReward} */ />
-                            <span>{h}: {m}: {s}</span>
+                        
                         </div>
                     }
 
