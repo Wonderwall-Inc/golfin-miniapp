cb6192dc0b47386e3fc5b87cdd2d4655c6d1b502
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-15 15:20:00 +0900

    buffer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
@@ -21,6 +22,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      buffer: 'buffer/'
     },
   },
   define: {
